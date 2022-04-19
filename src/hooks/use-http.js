c5cb787072f6551d5fdf58711d3d3fbb17489977
Roberta Sciortino <r.sciortino@mbdigitalinnovation.ch>
2022-04-19 16:18:36 +0200

    Using custom hook in App.js

---
+++ src/hooks/use-http.js
@@ -0,0 +1,47 @@
+import {useState} from "react";
+
+const useHttp = (requestConfig, applyData) => { //requestConfig è un oggetto che contiene le configurazioni - applyData applica i dati
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const sendRequest = async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(
+                requestConfig.url, {
+                    method: requestConfig.method ? requestConfig.method : 'GET',
+                    body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+                    headers: requestConfig.headers ? requestConfig.headers : {}
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+
+            applyData(data) //nell\'hook viene passata la funzione e i dati (sostituisce la parte sotto)
+
+            // const loadedTasks = [];
+            //
+            // for (const taskKey in data) {
+            //     loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+            // }
+            //
+            // setTasks(loadedTasks);
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    };
+
+    return {
+        isLoading,
+        error,
+        sendRequest
+    }
+}
+
+export default useHttp
